94dd8e08da82a4f02636f1723b92368fa7d1047a
Jym Paul Carandang <pauljym_carandang@yahoo.com>
2014-03-30 16:58:24 +0800

    Finished setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-from cx_Freeze import setup, Executable
 import sys
+from os import path
+from cx_Freeze import setup, Executable
+import re
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os", "pygame", "random", "time", "threading", "pickle"], "excludes": ["tkinter"], "include_files" = ["config.cfg", "resource"]}
 
-build_option = 0
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "Rotetris",
+        version = "0.1",
+        description = "CS 140 Rotetris",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
